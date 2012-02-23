605acda8c16eb9a77570b2432d9f787dade8b503
Rebekah David <rldavid@unca.edu>
2012-02-22 22:55:56 -0500

    applied scripts to other side, made color scheme less terrible

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -34,14 +34,13 @@ function main() {
 	 	
 	// }
 }
-
+		var firstTweetCount = 0;
 		var bartopCount = 0;
 		var secondTweetCount = 0;
 
 function firstSpotterObject() {
-		var firstTweetCount = 0;
-		var firstSize = 0;
 
+		var firstSize = 0;
 
 		var first = new Spotter("twitter.search",
 						{q:$("#term1").val(), period:20},
@@ -51,13 +50,12 @@ function firstSpotterObject() {
 				first.register(function(tweet) {	
 					var color;
 					firstSize = firstSize + 1;
-//					firstTweetCount = firstTweetCount + 1;
-					
-
 							
 					var profile_image = "<img src='"+tweet.profile_image_url+"' />";
 
 					var new_paragraph = $("<div class='boxleftyes'>"+'&nbsp;'+"</div>");
+					
+					//tweet text will popup when you hover over the representative box
 					var tweetpopup = $("<div class='tweetshow'><p>testing</p>"+profile_image+tweet.text+"</div>");
 					
   					new_paragraph.hide();
@@ -73,23 +71,17 @@ function firstSpotterObject() {
 						tweetpopup.remove();
 					});	
 						
-
+					//every time a new tweet box is added to the big section, it is added to the top section too. Goal is to show one search term's frequency compared to the other, but frequency of incoming tweets is too constant...
 					var new_leftBoxTop = $("<div class='boxleftyes'>"+'&nbsp;'+"</div>");
   					new_leftBoxTop.hide();
 					bartopCount = bartopCount + 1;
  					$("#bartop").prepend(new_leftBoxTop);
  					new_leftBoxTop.fadeIn();
 					
-					
-					// if(tweet.text.match($("#term1").val())) {
-					//     color = "'boxleftyes'";
-					// } else {
-					//     color = "'boxleftno'";
-					// }
-					
-					if (firstTweetCount === 20)	{
+					//only allow 100 tweet color boxes at once
+					if (firstTweetCount === 100)	{
 					 	$("#search1 div:first-child").remove();
-					 	firstTweetCount = 19;
+					 	firstTweetCount = 99;
 					 }
 
 				});
@@ -109,15 +101,27 @@ function secondSpotterObject() {
 				second.register(function(tweet) {	
 					var color;
 					secondSize = secondSize + 1;
-//					firstTweetCount = firstTweetCount + 1;
 
 					var profile_image = "<img src='"+tweet.profile_image_url+"' />";
 
+
 					var new_paragraph = $("<div class='boxrightyes'>"+'&nbsp;'+"</div>");
+					
+					//tweet text will popup when you hover over the representative box
+					var tweetpopup = $("<div class='tweetshow'><p>testing</p>"+profile_image+tweet.text+"</div>");
+					
   					new_paragraph.hide();
-					secondTweetCount = secondTweetCount + 1;
+					firstTweetCount = firstTweetCount + 1;
  					$("#search2").append(new_paragraph);
  					new_paragraph.fadeIn();
+					$(new_paragraph).mouseenter(function () {
+							tweetpopup.hide();
+							$(new_paragraph).append(tweetpopup);
+							tweetpopup.fadeIn();
+					});	
+					$(new_paragraph).mouseleave(function() {
+						tweetpopup.remove();
+					});
 
 					var new_rightBoxTop = $("<div class='boxrightyes'>"+'&nbsp;'+"</div>");
   					new_rightBoxTop.hide();
@@ -126,23 +130,14 @@ function secondSpotterObject() {
  					new_rightBoxTop.fadeIn();
 
 
-					// if(tweet.text.match($("#term1").val())) {
-					//     color = "'boxleftyes'";
-					// } else {
-					//     color = "'boxleftno'";
-					// }
-
-					if (secondTweetCount === 300)	{
+					if (secondTweetCount === 100)	{
 					 	$("#search2 div:first-child").remove();
-					 	secondTweetCount = 299;
+					 	secondTweetCount = 99;
 					 }
 						
 				});
 		second.start();
 };
-
-							//starts our Spotter constructor!
-
 
 
 $(document).ready(function() {
